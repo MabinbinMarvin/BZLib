fc42739bfebcfbab28811bc7a43d77eec588963e
binbinma <binbinma@mabinbindeMacBook-Pro.local>
2015-11-29 08:33:58 +0800

    Initial commit

---
+++ Example/BZLib/BZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BZAppDelegate.h
+//  BZLib
+//
+//  Created by binbinma on 11/29/2015.
+//  Copyright (c) 2015 binbinma. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BZLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BZLib
+//
+//  Created by binbinma on 11/29/2015.
+//  Copyright (c) 2015 binbinma. All rights reserved.
+//
+
+@import UIKit;
+#import "BZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BZAppDelegate class]));
+    }
+}
